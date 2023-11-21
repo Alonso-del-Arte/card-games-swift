4e33923df9f5c38c979859a8fea0e85028756d1b
Alonso del Arte <alonso.delarte@gmail.com>
2023-11-21 15:56:11 -0500

    New class, new test class

---
+++ PlayingCards/PlayingCard.swift
@@ -0,0 +1,20 @@
+//
+//  PlayingCard.swift
+//  CardGames
+//
+//  Created by Alonso del Arte on 11/21/23.
+//
+
+import Foundation
+
+struct PlayingCard: CustomStringConvertible {
+    
+    let rank: Rank
+    
+    let suit: Suit
+    
+    let description: String = "SORRY, NOT IMPLEMENTED YET"
+    
+    func shorthand() -> String {"SORRY, NOT IMPLEMENTED YET"}
+    
+}
