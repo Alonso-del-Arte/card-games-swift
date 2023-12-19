33bfc23640fc8b61a9147a2a27d607d5f2c73f7a
Alonso del Arte <alonso.delarte@gmail.com>
2023-12-18 21:51:34 -0500

    New class and new trait

---
+++ PlayingCards/CardProvider.swift
@@ -0,0 +1,12 @@
+//
+//  CardProvider.swift
+//  CardGames
+//
+//  Created by Alonso del Arte on 12/18/23.
+//
+
+import Foundation
+
+protocol CardProvider {
+    
+}
---
+++ PlayingCards/Deck.swift
@@ -0,0 +1,12 @@
+//
+//  Deck.swift
+//  CardGames
+//
+//  Created by Alonso del Arte on 12/18/23.
+//
+
+import Foundation
+
+struct Deck : CardProvider {
+    
+}
