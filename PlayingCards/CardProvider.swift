aef775660cad49f90ac9113c25f7e7b3936d09c3
Alonso del Arte <alonso.delarte@gmail.com>
2023-12-19 21:44:06 -0500

    Added two functions

--- PlayingCards/CardProvider.swift
+++ PlayingCards/CardProvider.swift
@@ -9,4 +9,13 @@ import Foundation
 
 protocol CardProvider {
     
+    /// Determines whether or not this card provider can give a card.
+    /// - Returns: True if a card can be given, false if it has run out of
+    /// cards.
+    func hasNext() -> Bool
+    
+    /// Gives a playing card, provided there are cards left to give.
+    /// - Returns: A playing card. For example, Three of Spades.
+    func getNextCard() -> PlayingCard
+    
 }
--- PlayingCards/Deck.swift
+++ PlayingCards/Deck.swift
@@ -9,4 +9,12 @@ import Foundation
 
 struct Deck : CardProvider {
     
+    // TODO: Write tests for this
+    func hasNext() -> Bool { false }
+    
+    // TODO: Write tests for this
+    func getNextCard() -> PlayingCard {
+        PlayingCard(rank: .ace, suit: .clubs)
+    }
+    
 }
